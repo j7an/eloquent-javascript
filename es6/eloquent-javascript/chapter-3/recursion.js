caa9068a6711b785715babb065e905e32af4adfc
j7an <jiatan24@gmail.com>
2016-04-26 05:46:30 +0000

    modified with eslint rules

--- es6/eloquent-javascript/chapter-3/recursion.js
+++ es6/eloquent-javascript/chapter-3/recursion.js
@@ -17,13 +17,13 @@ fix this?
 */
 
 function isEven(num) {
-    if (num == 0)
+    if (num === 0) {
         return true;
-    else if (num == 1)
+    } else if (num === 1) {
         return false;
-    else if (num < 0)
+    } else if (num < 0) {
         return isEven(num + 2);
-    else return isEven(num - 2);
+    } else return isEven(num - 2);
 }
 
 console.log(isEven(-75));
