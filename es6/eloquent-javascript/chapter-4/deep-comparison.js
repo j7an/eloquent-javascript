ac22ae2a1084c811e8dc6db74aab580316509dc7
j7an <jiatan24@gmail.com>
2016-05-05 02:00:52 +0000

    missing typeof, fixed eslint errors

--- es6/eloquent-javascript/chapter-4/deep-comparison.js
+++ es6/eloquent-javascript/chapter-4/deep-comparison.js
@@ -14,20 +14,28 @@ also produces "object".
 */
 
 function deepEqual(x, y) {
-    if ((x == "object" && x != null) && (y == "object" && y != null)) {
-        if (Object.keys(x).length != Object.keys(y).length) {
+    if ((typeof x === 'object' && x != null) && (typeof y === 'object' && y != null)) {
+        if (Object.keys(x).length !== Object.keys(y).length) {
             return false;
         } else {
-            for (let xProp in x) {
-                for (let yProp in y) {
-                    if (xProp.value === yProp.value) {
-                        //return true;
-                    }
+            for (let prop in x) {
+                if (deepEqual(x[prop], y[prop]) === false) {
+                    return false;
                 }
             }
+            return true;
         }
-    }
-    else {
+    } else {
         return x === y;
     }
 }
+
+console.log(deepEqual(4, 4)); // true
+console.log(deepEqual(4, null)); // false
+console.log(deepEqual(0, undefined)); // false
+console.log(deepEqual(null, undefined)); // false
+console.log(deepEqual(null, null)); // true
+const obj = { here: { is: 'an' }, object: 2 };
+console.log(deepEqual(obj, obj)); // true
+console.log(deepEqual(obj, { here: 1, object: 2 })); // false
+console.log(deepEqual(obj, { here: { is: 'an' }, object: 2 })); // true
