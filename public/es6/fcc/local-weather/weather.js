325759eb21aff1ab43f4de92e776c9ca32873351
j7an <jiatan24@gmail.com>
2016-05-16 07:33:13 +0000

    fixed undefined geolocation getting passed to getWeather before being set

--- public/es6/fcc/local-weather/weather.js
+++ public/es6/fcc/local-weather/weather.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     console.log('ready!');
     ipFindMe();
+    //geoFindMe();
 });
 
 //var x = document.getElementById("weather");
@@ -16,7 +17,7 @@ var weatherCondition = {"coord": {"lon":-115.24,"lat":36.03},
     "name":"Enterprise",
     "cod":200};
 
-var ipLocation = {"as":"AS22773 Cox Communications Inc.","city":"Las Vegas","country":"United States","countryCode":"US","isp":"Cox Communications","lat":32.0625,"lon":-114.3192,"org":"Cox Communications","query":"68.96.103.175","region":"NV","regionName":"Nevada","status":"success","timezone":"America/Los_Angeles","zip":"89148"};
+var ipLocation;// = {"as":"AS22773 Cox Communications Inc.","city":"Las Vegas","country":"United States","countryCode":"US","isp":"Cox Communications","lat":32.0625,"lon":-114.3192,"org":"Cox Communications","query":"68.96.103.175","region":"NV","regionName":"Nevada","status":"success","timezone":"America/Los_Angeles","zip":"89148"};
 
 var weatherClass
 var weatherDescription //= weatherCondition.weather[0].description;
@@ -25,6 +26,7 @@ var weatherLocation
 var tempKelvin
 var tempCelsius
 var tempFahrenheit
+var outputLocation = document.getElementsByClassName("location");
 var outputTemp = document.getElementsByClassName("temp");
 
 // function geoFindMe() {
@@ -41,12 +43,12 @@ var outputTemp = document.getElementsByClassName("temp");
         
 //         output.innerHTML = '<p>Latitude is ' + latitude + '° <br> Longitude is ' + longitude + '° </p>';
         
-//         var img = new Image();
-//         img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+//         // var img = new Image();
+//         // img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
         
 //         output.appendChild(img);
         
-//         console.log(getWeather(latitude, longitude));
+//         getWeather(latitude, longitude);
 //     }
     
 //     function error() {
@@ -60,33 +62,41 @@ var outputTemp = document.getElementsByClassName("temp");
 // }
 
 function ipFindMe() {
-    var latitude = ipLocation.lat;
-    var longitude = ipLocation.lon;
+    // var latitude; //= ipLocation.lat;
+    // var longitude; //= ipLocation.lon;
     //var output = document.getElementById("location");
-    var outputLocation = document.getElementsByClassName("location");
+    //var outputLocation = document.getElementsByClassName("location");
     var myXMLHttpRequest = new XMLHttpRequest();
-    var url = 'http://ip-api.com/json'
+    var url = 'http://ip-api.com/json';
     
     myXMLHttpRequest.onreadystatechange = function() {
         if (myXMLHttpRequest.readyState === 4 && myXMLHttpRequest.status === 200) {
             ipLocation = JSON.parse(myXMLHttpRequest.responseText);
             //var myIpJSON = JSON.stringify(myIpObject);
-            latitude = ipLocation.lat;
-            longitude = ipLocation.lon;
+            // latitude = ipLocation.lat;
+            // longitude = ipLocation.lon;
             weatherLocation = ipLocation.city + ", " + ipLocation.region;
             
-            //console.log(ipLocation);
+            console.log(ipLocation);
             //console.log(myIpJSON);
             
             //output.innerHTML = '<p>Latitude is ' + latitude + '° <br> Longitude is ' + longitude + '° </p>';
             outputLocation[0].innerHTML = weatherLocation;
+            // $(".location").html(function() {
+            //     return weatherLocation;
+            // });
+            //$(".location").html("<p>" + weatherLocation + "</p>");
+            //$(".location").append(document.createTextNode(weatherLocation));
+            
+            //console.log(getWeather(latitude, longitude));
+            //console.log(ipLocation.lat, ipLocation.lon);
+            getWeather(ipLocation.lat, ipLocation.lon);
         }
     }
     
     myXMLHttpRequest.open("GET", url, true);
     myXMLHttpRequest.send();
-    //console.log(getWeather(latitude, longitude));
-    getWeather(latitude, longitude);
+
 }
 
 function getWeather(latitude, longitude) {
@@ -100,13 +110,13 @@ function getWeather(latitude, longitude) {
     
     myXMLHttpRequest.onreadystatechange = function() {
         if (myXMLHttpRequest.readyState === 4 && myXMLHttpRequest.status === 200) {
-            var myObject = JSON.parse(myXMLHttpRequest.responseText);
-            var myJSON = JSON.stringify(myObject);
+            //var myObject = JSON.parse(myXMLHttpRequest.responseText);
+            //var myJSON = JSON.stringify(myObject);
             //console.log(myObject);
             //console.log(myJSON);
             
             weatherCondition = JSON.parse(myXMLHttpRequest.responseText);
-            //console.log(weatherCondition);
+            console.log(weatherCondition);
             
             weatherId = weatherCondition.weather[0].id;
             weatherClass = "wi wi-owm-" + weatherId;
@@ -117,9 +127,9 @@ function getWeather(latitude, longitude) {
             tempFahrenheit = Math.round((tempKelvin - 273.15) * 9/5 + 32) + " °F";
             //output.innerHTML = '<p>Kelvin is ' + tempKelvin + '°<br>Celsius is ' + tempCelsius + '°<br>Fahrenheit is ' + tempFahrenheit + '°</p>';
             $("i").removeClass().addClass(weatherClass);
-            outputCondition[0].innerHTML = weatherDescription;
-            outputTemp[0].innerHTML = tempCelsius;// + "°";
-            console.log(weatherId, weatherDescription, weatherClass);
+            outputCondition[0].innerHTML = "<p>" + weatherDescription + "</p>";
+            outputTemp[0].innerHTML = "<p>" + tempCelsius + "</p>";
+            //console.log(weatherId, weatherDescription, weatherClass);
         }
     }
     
@@ -135,9 +145,9 @@ var showFahrenheit = false;
 function toggleTemp() {
     showFahrenheit = !showFahrenheit;
     if (showFahrenheit) {
-        outputTemp[0].innerHTML = tempFahrenheit;
+        outputTemp[0].innerHTML = "<p>" + tempFahrenheit + "</p>";
     } else {
-        outputTemp[0].innerHTML = tempCelsius;
+        outputTemp[0].innerHTML = "<p>" + tempCelsius + "</p>";
     }
 }
 
